c3507f8978377e03a4f9b49ad870965c8d0f75b5
Alan <acasasar@everis.com>
2019-07-05 18:11:07 +0200

    [FEATURE][APP ASSEMBLY] App assembly was added

---
+++ Clima-Viper/AppAssembly.swift
@@ -0,0 +1,16 @@
+//
+//  AppAssembly.swift
+//  Clima-Viper
+//
+//  Created by Casa on 05/07/2019.
+//  Copyright © 2019 Alan Casas. All rights reserved.
+//
+
+import UIKit
+
+final class AppAssembly {
+    
+    private (set) lazy var window = UIWindow(frame: UIScreen.main.bounds)
+    private (set) lazy var navigationController = UINavigationController()
+//    private (set) lazy var coreAssembly = CoreAssembly(navigationController: navigationController)
+}
